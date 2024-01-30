23c8c6dbb714b65bc6188f391c45f1e4e844878c
viamill <126583359+viamill@users.noreply.github.com>
2024-01-30 18:11:09 -0500

    DemoFinal

--- data.gd
+++ data.gd
@@ -1,5 +1,7 @@
 extends Node
 
+#data.gd always runs even between scene transitions
+#This file helps to save different values during these scene transitions
 var player_dir: Vector2
 var player_cur_hp: int
 var is_door_open
