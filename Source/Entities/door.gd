1c741adb2a67cc11682837d9d27e3edb552fb26b
viamill <126583359+viamill@users.noreply.github.com>
2024-02-20 21:38:51 -0500

    Skeleton overhaul

--- Source/Entities/door.gd
+++ Source/Entities/door.gd
@@ -1,9 +1,9 @@
 extends StaticBody2D
 
 #export variables
-@export var next_room: String = ""
-@export var dir: Vector2
-@export var door_num: int
+@export var next_room: String = "" #path to the scene of the room the door leads to
+@export var dir: Vector2 #direction the player is going when they enter they door -- (0, -1) = North, (0, 1) = South, (-1, 0) = West, (1, 0) = East
+@export var door_num: int #index of the door in the is_door_open variable in data.gd. Doors that are connected have the same index
 
 #variables
 @onready var entered: bool = false
@@ -28,11 +28,16 @@ func _ready():
 func _physics_process(_delta):
 	if(open and player.dir == dir and exit_trigger.overlaps_area(player.hurtbox)): scene_transition()
 	
-#func open_door():
-#	open = true
-#	sprite.frame = 1
+func open_door():
+	open = true
+	sprite.frame = 1
+	
+func close_door():
+	open = false
+	sprite.frame = 0
 
 func scene_transition():
+	get_tree().call_group("Skeleton", "change_state", 1)
 	exit_trigger.set_deferred("disabled", true)
 	collision.set_deferred("disabled", true)
 	Data.player_dir = dir
